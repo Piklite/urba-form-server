0723832279344e47c6fceaa8dda02d2afb401628
Grégory Da Silva <gregory.dasilva@operis.fr>
2022-02-16 11:26:04 +0100

    port in .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfig from './config/app.config';
+
+const port = appConfig().port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
